cb2f4e38d1c997fde69d622513fd4dd061dad1c7
Martin Jerling <martin.jerling@u-blox.com>
2014-12-19 11:30:44 +0100

    Release v0.9.0.0 for ELIN-W16

--- scripts/ublox-poky-setup.sh
+++ scripts/ublox-poky-setup.sh
@@ -26,10 +26,10 @@ branch=daisy
 
 rev_poky=daisy-11.0.1
 rev_meta_openembedded=518cd119a8724dbd485a5d5c5047de59e8fbd652
-rev_meta_ublox=origin/$branch
-rev_meta_ublox_extras=origin/$branch
+rev_meta_ublox=elin-w16_v0.9.0.0
+rev_meta_ublox_extras=elin-w16_v0.9.0.0
 
-backup_suffix="backup-$(date +%F)"
+backup_suffix="backup-$(date +%F-%H%M%S)"
 
 print_usage() {
 	echo "Usage: `basename $0` [-f][-h]"
@@ -79,7 +79,7 @@ git_setup() {
 		git branch $git_branch_mv $branch "$branch-$backup_suffix"
 		git checkout -b $branch $tag
 	fi
-		
+
 	cd ..
 }
 
